55da659d5450432d37d808d63960a1c5a9e4f569
Matthew Dolan <matthew@rocketplantech.com>
2023-04-26 10:48:49 -0700

    Add random endpoint

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World", "data": 0}
+
+@app.get("/random")
+async def get_random():
+    rn: int = random.randint(0, 100)
+    return {"number": rn, "limit": 100}
+
+@app.get("/random/{limit}")
+async def get_random(limit: int):
+    rn: int = random.randint(0, limit)
+    return {"number": rn, "limit": limit}
